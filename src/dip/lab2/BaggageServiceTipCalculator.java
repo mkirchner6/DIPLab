b5f282eaf3e145da4faeb42882d9de45826c01d9
mattkirchner <mattkirchner@10.30.21.102>
2017-09-26 15:28:23 -0500

    Fixed baggageCalc in lab 2

---
+++ src/dip/lab2/BaggageServiceTipCalculator.java
@@ -0,0 +1,114 @@
+package dip.lab2;
+
+/**
+ * An example low-level class. Does this class definition follow the DIP?
+ * If not, fix it.
+ *
+ * Any other best practice violations? Fix them too.
+ *
+ * @author your name goes here
+ */
+public class BaggageServiceTipCalculator {
+    private double goodRate = 0.20;
+    private double fairRate = 0.15;
+    private double poorRate = 0.10;
+
+    private double baseTipPerBag;
+    private int bagCount;
+    
+    private ServiceQuality serviceQuality;
+
+    public BaggageServiceTipCalculator(ServiceQuality q, int bags) {
+        this.setServiceRating(q); // perform validation
+        this.setBagCount(bags);
+
+        baseTipPerBag = 1.00; // set default value
+    }
+
+    public final double getTipForBaggeHandler() {
+        double tip = 0.00; // always initialize local variables
+
+        switch(serviceQuality) {
+            case GOOD:
+                tip = baseTipPerBag * bagCount * (1 + goodRate);
+                break;
+            case FAIR:
+                tip = baseTipPerBag * bagCount * (1 + fairRate);
+                break;
+            case POOR:
+                tip = baseTipPerBag * bagCount * (1 + poorRate);
+                break;
+        }
+
+        return tip;
+    }
+
+    public final void setServiceRating(ServiceQuality q) {
+        // No need to validate because enums provide type safety!
+        serviceQuality = q;
+    }
+
+    public final ServiceQuality getServiceQuality() {
+        return serviceQuality;
+    }
+
+    public final int getBagCount() {
+        return bagCount;
+    }
+
+    public final void setBagCount(int bagCount) {
+        if(bagCount < 0) {
+            throw new IllegalArgumentException(
+                    "bag count must be greater than or equal to zero");
+        }
+        this.bagCount = bagCount;
+    }
+
+    public final double getBaseTipPerBag() {
+        return baseTipPerBag;
+    }
+
+    public final void setBaseTipPerBag(double baseTipPerBag) {
+        if(baseTipPerBag < 0) {
+            throw new IllegalArgumentException(
+                    "error: base tip must be greater than or equal to zero");
+        }
+        this.baseTipPerBag = baseTipPerBag;
+    }
+
+    public final double getGoodRate() {
+        return goodRate;
+    }
+
+    public final void setGoodRate(double goodRate) {
+        if(goodRate < 0 || goodRate > 1){
+            throw new IllegalArgumentException("Must be a valid percent");
+        }
+        this.goodRate = goodRate;
+    }
+
+    public final double getFairRate() {
+        return fairRate;
+    }
+
+    public final void setFairRate(double fairRate) {
+        if(fairRate < 0 || fairRate > 1){
+            throw new IllegalArgumentException("Must be a valid percent");
+        }
+        this.fairRate = fairRate;
+    }
+
+    public final double getPoorRate() {
+        return poorRate;
+    }
+
+    public final void setPoorRate(double poorRate) {
+        if(poorRate < 0 || poorRate > 1){
+            throw new IllegalArgumentException("Must be a valid percent");
+        }
+        this.poorRate = poorRate;
+    }
+
+    
+
+}
